d77df09eab309ce87644cb6d0ace51301f0272fb
justin <justin@redhq.com.au>
2024-07-28 02:13:31 +0400

    chore: Update deployment script to include npm installation and build
    The deployment script has been modified to include the installation of npm dependencies and the execution of the `npm run build` command. This ensures that the necessary frontend assets are built and ready for production use.

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 echo "Running composer install..."
 composer install --no-dev --working-dir=/var/www/html
+
+echo "Running npm install..."
+npm install --prefix /var/www/html
+
+echo "Running npm run prod..."
+npm run build --prefix /var/www/html
 
 echo "Running migrations..."
 php artisan migrate --force
