037fe7f99402d48ac0dd962432289db2952a1ef5
justin <justin@redhq.com.au>
2024-07-28 02:14:23 +0400

    chore: Update deployment script to include cache clearing and optimization
    The deployment script has been modified to include clearing the cache and optimizing the application. This ensures that any cached data is cleared and the application is optimized for better performance.

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -12,5 +12,29 @@ npm install --prefix /var/www/html
 echo "Running npm run prod..."
 npm run build --prefix /var/www/html
 
+echo "Running cache clear..."
+php artisan cache:clear
+
+echo "Running config clear..."
+php artisan config:clear
+
+echo "Running route clear..."
+php artisan route:clear
+
+echo "Running view clear..."
+php artisan view:clear
+
+echo "Running config cache..."
+php artisan config:cache
+
+echo "Running route cache..."
+php artisan route:cache
+
+echo "Running view cache..."
+php artisan view:cache
+
+echo "Running optimize..."
+php artisan optimize
+
 echo "Running migrations..."
 php artisan migrate --force
